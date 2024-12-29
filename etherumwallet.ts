a1d396d3fccac742f6a4adbb877f1fa80e0c51de
Hrushikesh Roop <161935100+hrushikeshroop@users.noreply.github.com>
2024-12-29 22:36:08 +0530

    etherumwallet.ts

--- etherumwallet.ts
+++ etherumwallet.ts
@@ -7,6 +7,11 @@ console.log("Generated Mnemonic:", mnemonic);
 const seed = mnemonicToSeedSync(mnemonic);
 const hdNode = HDNodeWallet.fromSeed(seed);
 
+
+//path  for bitcoin : m/44'/0'/${i}
+// path for eth : m/44'/60'/${i}
+//path for sol : m/44'/150'/${i}
+
 for (let i = 0; i < 4; i++) {
     const wallet = hdNode.derivePath(`m/44'/60'/${i}`);
     console.log("Path:", wallet.path);
